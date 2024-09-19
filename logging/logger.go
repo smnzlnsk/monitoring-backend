5aac5d5ca6cfb135445c2189f42cdb44cd8f80aa
Simon Zelenski <simon.zelenski@gmail.com>
2024-09-19 16:50:42 +0200

    add log level setting to logger.go

--- logging/logger.go
+++ logging/logger.go
@@ -2,12 +2,17 @@ package logging
 
 import (
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 var Logger *zap.Logger
 
 func NewLogger() error {
-	l, err := zap.NewDevelopment()
+	config := zap.NewDevelopmentConfig()
+
+	config.Level = zap.NewAtomicLevelAt(zapcore.InfoLevel)
+
+	l, err := config.Build()
 	if err != nil {
 		return err
 	}
